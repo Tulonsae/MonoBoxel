9a80fee4bf9a6a6e284e12d9c0d42dae65b48320
MikeMatrix <m.braun92@gmail.com>
2012-02-12 19:25:19 +0100

    * Added missing Comments in MBPermissionManager.java

--- src/main/java/com/github/Monofraps/MonoBoxel/MBPermissionManager.java
+++ src/main/java/com/github/Monofraps/MonoBoxel/MBPermissionManager.java
@@ -6,17 +6,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 
 
+/**
+ * Managing all available Permissions for MonoBoxel.
+ * 
+ * @author MikeMatrix
+ */
 public class MBPermissionManager {
 	
 	private MonoBoxel	master	= null;
 	
+	/**
+	 * Enumeration of all Available Generic Permissions.
+	 * 
+	 * @author MikeMatrix
+	 * 
+	 */
 	public enum MBPermission {
-		CAN_CREATE_OWN(new Permission("monoboxel.boxel.create.own")),
-		CAN_CREATE_OTHERS(new Permission("monoboxel.boxel.create.other")),
-		CAN_CREATE_GROUP_BOXEL(new Permission("monoboxel.groupboxel.create")),
-		CAN_VISIT_OWN(new Permission("monoboxel.boxel.visit.own")),
-		CAN_VISIT_OTHERS(new Permission("monoboxel.boxel.create.other")),
-		CAN_VISIT_GROUP_BOXEL(new Permission("monoboxel.groupboxel.visit"));
+		CAN_CREATE_OWN(new Permission("monoboxel.boxel.create.own")), CAN_CREATE_OTHERS(
+				new Permission("monoboxel.boxel.create.other")), CAN_CREATE_GROUP_BOXEL(
+				new Permission("monoboxel.groupboxel.create")), CAN_VISIT_OWN(
+				new Permission("monoboxel.boxel.visit.own")), CAN_VISIT_OTHERS(
+				new Permission("monoboxel.boxel.create.other")), CAN_VISIT_GROUP_BOXEL(
+				new Permission("monoboxel.groupboxel.visit"));
 		
 		private final Permission	perm;
 		
@@ -33,24 +44,63 @@ public class MBPermissionManager {
 		master = plugin;
 	}
 	
+	/**
+	 * Check if a Player has a specific Generic Permission.
+	 * 
+	 * @param player
+	 * @param permission
+	 * @return true if Player has the Permission, false if Player doesn't have the Permission
+	 */
 	public boolean hasPermission(Player player, MBPermission permission) {
 		return this.hasPermission(player, permission.getPermission());
 	}
 	
+	/**
+	 * Check if a Player has a specific Permission.
+	 * 
+	 * @param player
+	 * @param permission
+	 * @return true if Player has the Permission, false if Player doesn't have the Permission
+	 */
 	public boolean hasPermission(Player player, Permission permission) {
 		return player.hasPermission(permission);
 	}
 	
+	/**
+	 * Check if a Player has a specific Permission.
+	 * 
+	 * @param player
+	 * @param permission
+	 * @return true if Player has the Permission, false if Player doesn't have the Permission
+	 */
+	public boolean hasPermission(Player player, String permissionNode) {
+		return player.hasPermission(permissionNode);
+	}
+	
+	/**
+	 * Extra implementation of per-Player check, since enum's do not support Variations.
+	 * 
+	 * @TODO: Check if there is a possibility to implement this in the current framework.
+	 * @param player
+	 * @param boxelName
+	 * @return true if Player has the Permission to visit the specified Boxel, false if Player
+	 *         doesn't have the Permission to visit the specified Boxel
+	 */
 	public boolean canVisitOtherBoxel(Player player, String boxelName) {
-		if (boxelName.startsWith(master.getBoxelPrefix())) boxelName = boxelName
-				.substring(master.getBoxelPrefix().length());
+		if (boxelName.startsWith(master.getBoxelPrefix()))
+			boxelName = boxelName.substring(master.getBoxelPrefix().length());
 		
 		return this.hasPermission(player, new Permission(
 				"monoboxel.boxel.visit." + boxelName));
 	}
 	
-	public void SendNotAllowedMessage(CommandSender sender)
-	{
+	/**
+	 * Send Notification to the command executer, that he lacks the Permission, to do, what he tried
+	 * to do.
+	 * 
+	 * @param sender
+	 */
+	public void SendNotAllowedMessage(CommandSender sender) {
 		sender.sendMessage("A divine voice says: 'You are not allowd to do this!'");
 	}
 }
